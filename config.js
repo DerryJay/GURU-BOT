daffd783ecf575c2377eb41ce6f6231a8eb7f539
DerryJay <99408987+DerryJay@users.noreply.github.com>
2024-03-22 16:46:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['2348100332985', 'JAYY', true],
+  ['2349019406102', 'JAYY', true],
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2348100322985','2348100322985'] 
-global.prems = ['2348100322985', '2348100322985', '2348100322985']
-global.allowed = ['2348100322985', '2348100322985', '2348100322985']
+global.mods = ['2349019406102','2349136546278'] 
+global.prems = ['2349019406102', '2349136546278', '2348100322985']
+global.allowed = ['2349019406102', '2349136546278', '2348100322985']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
